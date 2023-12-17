21f512b3464dfea3be7c7c62bff7f11a3df121c7
Tamnai Mitchelle <mitchellecheptoek@gmail.com>
2023-12-17 23:11:35 +0300

    Modified file index.js

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  if (someValue < 400) { 
+    console.log("This one is on me")
+  
+  }else if (someValue > 2000) {
+    console.log("I will gladly take your thirty bucks")
+  
+  }else (someValue > 2500) 
+    console.log("No can do")
+  
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(checkCity){
   // Write your code here!
+  return checkCity == 'NYC'?'Ok, sounds good.': 'No go.'
+
+
 }
 
 function switchOnCharmFromTip(){
   // Write your code here!
+  switch (charmFromTip) {
+    case 'generous': 
+        console.log ("Thank you so much")
+
+    case 'not as generous':
+        console.log ("Thank you")
+  
+    default :
+        console.log ("Bye")
+  }
+
+
 }
+
